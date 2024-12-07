2dffe6a7bf62f01cedd2c4bd625605f70de3984d
Lê Thắng <lesythang2@gmail.com>
2024-12-07 15:42:10 +0700

    Test working routing and htaccess

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Báo Thủy lợi - Tin tức mới nhất, Việt Nam và thế giới</title>
+</head>
+<body>
+    <p>Web báo này đã chạy</p>
+</body>
+</html>
