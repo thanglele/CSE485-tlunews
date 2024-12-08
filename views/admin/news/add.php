d97a97581dfa050b4d5b5605f88ba436a1d761c6
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-08 18:05:44 +0700

    Xíu sửa tiếp

---
+++ views/admin/news/add.php
@@ -0,0 +1,6 @@
+<h1>Thêm danh mục mới</h1>
+<form method="POST">
+    <label for="name">Tên danh mục:</label>
+    <input type="text" name="name" id="name" required>
+    <button type="submit">Lưu</button>
+</form>
