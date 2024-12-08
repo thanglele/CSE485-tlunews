96eb2266d7fc7556126423287c6e8c4839801337
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-08 11:11:58 +0700

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -7,6 +7,7 @@ class Category{
     }
 }
 
+//Lấy danh sách
 public function getAllCategories(){
     $query = "SELECT * FROM categories";
     $stmt = $this->db->prepare($query);
@@ -14,11 +15,37 @@ public function getAllCategories(){
     return $stmt->fetchAll(PDO::FETCH_ASSOS);
 }
 
+//Lấy thông tin
 public function getAllCategoryById($id){
     $query = "SELECT * FROM categories WHERE id = :id";
     $stmt = $this->db->prepare($query);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
     return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+//Thêm
+public function addCategory($name){
+    $query = "INSERT INTO categories (name) VALUES (:name)";
+    $stmt = $this->db->prepare($query);
+    $stmt->bindParam(':name', $name);
+    return $stmt->execute();
+}
+
+//Sửa
+public function updateCategory($id, $name){
+    $query = "UPDATE categories SET name = :name WHERE id = :id";
+    $stmt = $this->db->prepare($query);
+    $stmt->bindParam(':id'. $id, PDO::PARAM_INT);
+    $stmt->bindParam(':name', $name);
+    return $stmt->execute();
+}
+
+//Xóa
+public function deleteCategory($id){
+    $query = "DELETE FROM categories WHERE id = :id";
+    $stmt = $this->db->prepare($query);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    return $stmt->execute();
 }
 ?>
