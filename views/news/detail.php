675bbe843617553dda74cbc867073d6d7c054ab2
AnhNgo127 <161997871+AnhNgo127@users.noreply.github.com>
2024-12-08 23:48:37 +0700

    aaa

--- views/news/detail.php
+++ views/news/detail.php
@@ -52,8 +52,7 @@
     <div class="card-body">
         <p class="text-muted">Ngày đăng: <?php echo htmlspecialchars($news['created_at']); ?></p>
         <hr>
-        <?php$a = APP_ROOT.'<?php echo htmlspecialchars($news['image']); ?>';?>
-        <img src="<?php echo $a?>" alt="" width="300">
+        <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/data/' . htmlspecialchars($news['image']); ?>" alt="News Image" width="300">
         <p><?php echo nl2br(htmlspecialchars($news['content'])); ?></p>
     </div>
     <div class="card-footer text-right">
